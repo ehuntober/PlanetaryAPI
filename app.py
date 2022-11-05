f16af53c7601f5eee129a5335dc1426160afa78e
ehuntober <ehuntercrypto@gmail.com>
2022-11-05 05:15:53 +0000

    new updatated

--- app.py
+++ app.py
@@ -1,16 +1,57 @@
+import email
 from flask import Flask , jsonify , request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column , Integer, String , Float
 import os
 from flask_marshmallow import Marshmallow
-
+from flask_jwt_extended import JWTManager, jwt_required , create_access_token
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///' + os.path.join(basedir,'planets.db')
+app.config['JWT_SECRET_KEY'] = 'super-secret'
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+jwt = JWTManager(app)
+
+# database models 
+class User(db.Model):
+    __tablename__ = 'users'
+    id = Column(Integer,primary_key=True)
+    first_name = Column(String)
+    last_name=Column(String)
+    email = Column(String,unique=True)
+    password = Column(String)
+
+
+class Planet(db.Model):
+    __tablename__ = 'planets'
+    planet_id= Column(Integer,primary_key=True)
+    planet_name = Column(String)
+    planet_type = Column(String)
+    home_star = Column(String)
+    mass = Column(Float)
+    radius = Column(Float)
+    distance = Column(Float)
+
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id','first_name','last_name','email','password')
+
+
+class PlanetSchema(ma.Schema):
+    class Meta:
+        fields = ('planet_id','planet_name','planet_type','home_star','mass','radius','distance')
+
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+
+planet_schema = PlanetSchema()
+planets_schema = PlanetSchema(many=True)
 
 @app.cli.command('db_create')
 def db_create():
@@ -116,45 +157,26 @@ def register():
         db.session.add(user)
         db.session.commit()
         return jsonify(message="User created successfully"), 201
-        
 
 
-# database models 
-class User(db.Model):
-    __tablename__ = 'users'
-    id = Column(Integer,primary_key=True)
-    first_name = Column(String)
-    last_name=Column(String)
-    email = Column(String,unique=True)
-    password = Column(String)
+@app.route('/login', methods=['POST'])
+def login():
+    if request.is_json:
+        email = request.json['email']
+        password = request.json['password']
+    else:
+        email = request.form['email']
+        password = request.form['password']
+    
+    test = User.query.filter_by(email=email,password = password).first()
+    if test:
+        access_token = create_access_token(identity=email)
+        return jsonify(message='Login succeeded!', access_token=access_token),201
+    else:
+        return jsonify(message='Badd email or password'), 401        
 
 
-class Planet(db.Model):
-    __tablename__ = 'planets'
-    planet_id= Column(Integer,primary_key=True)
-    planet_name = Column(String)
-    planet_type = Column(String)
-    home_star = Column(String)
-    mass = Column(Float)
-    radius = Column(Float)
-    distance = Column(Float)
 
 
-class UserSchema(ma.Schema):
-    class Meta:
-        fields = ('id','first_name','last_name','email','password')
-
-
-class PlanetSchema(ma.Schema):
-    class Meta:
-        fields = ('planet_id','planet_name','planet_type','home_star','mass','radius','distance')
-
-
-user_schema = UserSchema()
-users_schema = UserSchema(many=True)
-
-
-planet_schema = PlanetSchema()
-planets_schema = PlanetSchema(many=True)
 if __name__ == '__main__':
     app.run(debug=True)
