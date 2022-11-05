555743db6b4562801b11e7b3307e3e17fefc42f2
ehuntober <ehuntercrypto@gmail.com>
2022-11-05 09:27:45 +0000

    environmenet variable added

--- app.py
+++ app.py
@@ -1,19 +1,36 @@
-import email
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
 from flask import Flask , jsonify , request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column , Integer, String , Float
 import os
 from flask_marshmallow import Marshmallow
 from flask_jwt_extended import JWTManager, jwt_required , create_access_token
+from flask_mail import Mail , Message
+
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///' + os.path.join(basedir,'planets.db')
 app.config['JWT_SECRET_KEY'] = 'super-secret'
 
+
+app.config['MAIL_SERVER']='smtp.mailtrap.io'
+app.config['MAIL_PORT'] = 2525
+app.config['MAIL_USERNAME']= os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD']= os.getenv('MAIL_PASSWORD')
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+
+
+
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 jwt = JWTManager(app)
+mail = Mail(app)
 
 # database models 
 class User(db.Model):
@@ -176,7 +193,18 @@ def login():
         return jsonify(message='Badd email or password'), 401        
 
 
+@app.route('/retrieve_password/<string:email>',methods=['GET'])
+def retrieve_password(email: str):
+    user = User.query.filter_by(email=email).first()
+    if user:
 
+        msg = Message('Your planetary API password is ' + user.password,
+                        sender='admin@planetary',
+                        recipients=[email])
+        mail.send(msg)
+        return jsonify(message='Password sent to' + email)
+    else:
+        return jsonify(message="That email doesn't exist")
 
 if __name__ == '__main__':
     app.run(debug=True)
