17f009354dd8819ce728c615b2ca23f6b4617ec0
ehuntober <ehuntercrypto@gmail.com>
2022-11-06 15:30:43 +0000

    new update

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+from email import message
 import os
+from unittest import result
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -120,45 +122,6 @@ def db_seed():
     print('Database seeded')
 
 
-@app.route('/')
-def hello_world():
-    return "hello world"
-
-
-@app.route('/super_simple')
-def super_simple():
-    return jsonify(message = 'Hello from the Planetary API'), 200
-
-@app.route('/not_found')
-def not_found():
-    return jsonify(message='That resource was not found'), 404
-
-
-@app.route('/parameters')
-def parameters():
-    name = request.args.get('name')
-    age = int(request.args.get('age'))
-    if age < 18:
-        return jsonify(message="Sorry " + name + ", You are nt old enough."), 401
-    else:
-        return jsonify(message="Welcome " + name + ", You are old enought!")
-
-
-@app.route('/url_variables/<string:name>/<int:age>')
-def url_variables(name: str, age: int):
-    if age < 18:
-            return jsonify(message="Sorry " + name + ", You are nt old enough."), 401
-    else:
-        return jsonify(message="Welcome " + name + ", You are old enought!")
-
-
-@app.route('/planets',methods=['GET'])
-def planets():
-    planets_list = Planet.query.all()
-    result = planets_schema.dump(planets_list)
-    return jsonify(result.data)
-
-
 @app.route('/register',methods=['POST'])
 def register():
     email = request.form['email']
@@ -204,7 +167,71 @@ def retrieve_password(email: str):
         mail.send(msg)
         return jsonify(message='Password sent to' + email)
     else:
-        return jsonify(message="That email doesn't exist")
+        return jsonify(message="That email doesn't exist"),401
+
+
+# Planet Api route >>>>
+
+@app.route('/planets',methods=['GET'])
+def planets():
+    planets_list = Planet.query.all()
+    result = planets_schema.dump(planets_list)
+    return jsonify(result)
+
+
+@app.route('/planet_details/<int:planet_id>', methods=['GET'])
+def planet_details(planet_id: int):
+    planet = Planet.query.filter_by(planet_id=planet_id).first()
+    if planet:
+        result = planet_schema.dump(planet)
+        return jsonify(result)
+
+    else:
+        return jsonify(message="That Planet does not exist"),404
+
+
+@app.route('/add_planet', methods=['POST'])
+@jwt_required()
+def add_planet():
+    planet_name = request.form['planet_name']
+    test = Planet.query.filter_by(planet_name=planet_name).first()
+    if test:
+        return jsonify('There is already a planet by that name'), 409
+    else:
+        planet_type = request.form['planet_type']
+        home_star = request.form['home_star']
+        mass = float(request.form['mass'])
+        radius = float(request.form['radius'])
+        distance = float(request.form['distance'])
+
+        new_planet = Planet(planet_name=planet_name,
+                            planet_type=planet_type,
+                            home_star=home_star,
+                            mass=mass,
+                            radius=radius,
+                            distance = distance)
+        db.session.add(new_planet)
+        db.session.commit()
+        return jsonify(message='You added a planet'), 201
+
+
+@app.route('/update_planet',methods=['PUT'])
+@jwt_required()
+def update_planet():
+    planet_id = int(request.form['planet_id'])
+    planet = Planet.query.filter_by(planet_id=planet_id).first()
+    if planet:
+        planet.planet_name = request.form['planet_name']
+        planet.planet_type = request.form['planet_type']
+        planet.home_star = request.form['home_star'] 
+        planet.mass = float(request.form['mass'])
+        planet.radius = float(request.form['radius'])
+        planet.distance = float(request.form['distance'])
+        db.session.commit()
+        return jsonify(message="You updated a planet"), 202
+    else:
+        return jsonify(message="that planet does not exist"), 404
+
 
 if __name__ == '__main__':
     app.run(debug=True)
